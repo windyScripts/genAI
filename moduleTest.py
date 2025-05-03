7a4af8a948f451d7f22611f56285caa41cab8e96
Aravind <aravindshenoy@ideaclan.com>
2025-05-03 15:34:02 +0530

    Up till exception handling

---
+++ moduleTest.py
@@ -0,0 +1,7 @@
+from package.maths import *
+from package.subpackages import mult
+
+print(subtraction(4,2))
+print(addition(2,4))
+print(mult.multiply(2,5))
+
---
+++ package/__init__.py
@@ -0,0 +1,9 @@
+""" 
+__init__.py is a special file used in p
+ython to define packages and initialize 
+their namespace.
+
+define modules using this.
+
+"""
+
